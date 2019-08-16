6eb956efa22395cf472a28790b674f44ebc50025
ilja <i-l-j-a@arcor.de>
2019-08-16 18:19:44 +0200

    läuft auf server

--- Anmeldung/forms.py
+++ Anmeldung/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.forms import ModelForm
 
+from django.core.mail import send_mail
+
 from .models import Teilnahme
 
 
@@ -14,3 +16,15 @@ class TeilnahmeForm(ModelForm):
         widgets = {
             'geburtsdatum': DateInput(),
         }
+
+    def save(self):
+        """ Mail versenden beim Speichern """
+        instance = super().save()
+        send_mail(
+            '[orpheus-verein.de] Deine Anmeldung',
+            instance.bestaetigungstext,
+            'seminar@orpheus-verein.de',
+            [self.cleaned_data['email']],
+            fail_silently=False,
+        )
+        return instance
--- Anmeldung/models.py
+++ Anmeldung/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 class Teilnahme(TimeStampedModel):
     """ Klasse für Teilnahme eines Schülers """
-    vorname = models.CharField(max_length=144, blank=False)
-    nachname = models.CharField(max_length=144, blank=False)
-    geschlecht = models.CharField(max_length=1, choices=(('m', 'm'), ('w', 'w')), blank=False)
-    email = models.EmailField(max_length=144, unique=True, blank=False)
+    vorname = models.CharField(max_length=144, verbose_name="Vorname", blank=False)
+    nachname = models.CharField(max_length=144, verbose_name="Nachname", blank=False)
+    geschlecht = models.CharField(max_length=1, verbose_name="Geschlecht", choices=(('m', 'm'), ('w', 'w')), blank=False)
+    email = models.EmailField(max_length=144, verbose_name="eMail", unique=True, blank=False)
     geburtsdatum = models.DateField(blank=False)
     telefon = models.CharField(max_length=144, verbose_name="Telefonnummer zur Planung der Anreise", blank=True)
     notfallnummer = models.CharField(max_length=144, verbose_name="Telefonnummer der Eltern für Notfälle", blank=False)
@@ -17,4 +17,31 @@ class Teilnahme(TimeStampedModel):
 
 
     def __str__(self):
-        return "%s %s" % (self.vorname, self.nachname)
+        return "%s %s, %s" % (self.vorname, self.nachname, self.email)
+
+    @property
+    def bestaetigungstext(self):
+        """ für Mailversand """
+        felder = [
+            self._meta.get_field(name)
+            for name in 'vorname, nachname, geburtsdatum, telefon, notfallnummer, essenswünsche, beeinträchtigungen, weitere_hinweise'.split(', ')
+        ]
+        daten = '\n'.join([
+            " - %s: %s" % (feld.verbose_name, getattr(self, feld.attname))
+            for feld in felder
+        ])
+
+        return """Guten Tag, {vorname} {nachname},
+
+Wir haben die folgenden Daten deiner Anmeldung gespeichert.
+Wenn du noch Fragen hast, oder sich etwas ändern sollte, antworte bitte auf diese eMail!
+
+Deine Anmeldung:
+
+{daten}
+
+Viele Grüße vom Orpheus e.V.
+""".format(
+            vorname=self.vorname,
+            nachname=self.nachname,
+            daten=daten)
